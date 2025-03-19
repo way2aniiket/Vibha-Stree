3c9c192b4468009aec1b71bdc51406a19ea743ff
Aniket Kumar <aniiket.ak@gmail.com>
2025-03-19 10:10:18 +0530

    uri updated

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@vibhastree.iflzw.mongodb.net/Vibha-Stree`)
+    `mongodb+srv://vibhastree:lbfru3leb1u3xzrk@vibhastree.iflzw.mongodb.net/Vibha-Stree`)
   .then(() => {
     console.log("Database is connected");
   })
