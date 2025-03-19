09cb7ca60d668e0ce81e64b83442f9a015d2843a
Aniket Kumar <aniiket.ak@gmail.com>
2025-03-19 10:19:40 +0530

    new fixes

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 mongoose
   .connect(
-    `mongodb+srv://VibhaStree:lBFru3LeB1u3xzRk@vibhastree.iflzw.mongodb.net/`)
+    `mongodb+srv://VibhaStree:lBFru3LeB1u3xzRk@vibhastree.iflzw.mongodb.net/Vibha-Stree`)
   .then(() => {
     console.log("Database is connected");
   })
