0e2c215a3d96d6db4424fdb725726a10f568e07c
Aniket Kumar <aniiket.ak@gmail.com>
2025-03-19 10:33:48 +0530

    mongoose connextion

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 mongoose
   .connect(
-    `mongodb+srv://VibhaStree:lBFru3LeB1u3xzRk@vibhastree.iflzw.mongodb.net/Vibha-Stree`)
+    `mongodb+srv://VibhaStree:lBFru3LeB1u3xzRk@vibhastree.iflzw.mongodb.net/Vibha-Stree`,{
+      serverSelectionTimeoutMS: 60000, // Increase timeout to 60 seconds
+      socketTimeoutMS: 45000, 
+    })
   .then(() => {
     console.log("Database is connected");
   })
